7ae8e6b15dc934c61668d66c6a411d38ffa85623
Danny <bublikplushka@yandex.ru>
2021-09-01 13:53:47 +0300

    Onegin. Finished sorting part

--- onegin/onegin.c
+++ onegin/onegin.c
@@ -1,2 +1,106 @@
 #include <onegin.h>
+#include <assert.h>
 
+void qsort (void* array, size_t n_memb,  size_t el_size,
+            int (*comparator) (const void*, const void*))
+{   
+    assert (array);
+    assert (comparator);
+
+    unsigned pivot_index = choose_pivot (n_memb);
+    unsigned left = paritation (array, n_memb, el_size, pivot_index, comparator);
+
+    if (left > 0)
+    {
+        qsort (array, left, el_size, comparator);
+    }
+
+    if (n_memb - left - 1 > 0)
+    {
+        qsort (array + (left + 1) * el_size, n_memb - left - 1, el_size, comparator);
+    }
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+unsigned paritation (void* array, size_t n_memb, size_t el_size, unsigned pivot_index, 
+                     int (*comparator) (const void*, const void*))
+{   
+    assert (array);
+    assert (comparator);
+
+    if (n_memb <= 1)
+    {
+        return;
+    }
+
+    void* pivot = array + pivot_index * el_size;
+    unsigned i = 1;
+    unsigned left = 0;
+
+    swap (array, pivot, el_size);
+    pivot = array;
+
+    for (i; i < n_memb; i++)
+    {
+        if (comparator (array + i * el_size, pivot) > 0)
+        {   
+            left++;
+            swap (array + left * el_size, array + i * el_size, el_size);
+        }
+    }
+
+    swap (pivot, array + left * el_size, el_size);
+
+    return left;
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+void swap (void* a, void* b, size_t el_size)
+{   
+    assert (a);
+    assert (b);
+    
+    char* a_arr = (char*) a;
+    char* b_arr = (char*) b;
+    char tmp = 0;
+    unsigned i = 0;
+
+    for (i; i < el_size; i++)
+    {
+        tmp = a_arr[i];
+        a_arr[i] = b_arr[i];
+        b_arr[i] = tmp;
+    }
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+unsigned choose_pivot (size_t n_memb)
+{
+    return n_memb / 2;
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+int lexicographic_comparator (const void* string_1, const void* string_2)
+{
+    string* str_1 = (string*) string_1;
+    string* str_2 = (string*) string_2;
+
+    unsigned minlen = (str_1->len < str_2->len)? str_1->len:str_2->len;
+    unsigned i = 0;
+
+    for (i; i < minlen; i ++)
+    {
+        if (str_1->start[i] != str_2->start[i])
+        {
+            break;
+        }
+    }
+
+    return (int) str_1->start[i] - (int) str_2->start[i];
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
--- onegin/onegin.h
+++ onegin/onegin.h
@@ -21,12 +21,15 @@ typedef struct string
 /**
 * @brief Функция qsort сортирует переданный массив.
 *   
-* @param [in] array Указатель на сортируемый массив.
-* @param [in] start Левая граница сортируемого участка.
-* @param [in] end Правая граница сортируемого участка.
-* @param [in] comp Функция для сравнения элементов массива.
+* @param [in] array Указатель на массив указателей для сортировки.
+* @param [in] n_memb Количество элементов в массиве.
+* @param [in] el_size Размер одного элемента в байтах.
+* @param [in] start Левая граница сортируемого участка (индекс элемента массива).
+* @param [in] end Правая граница сортируемого участка (индекс элемента массива).
+* @param [in] comparator Указатель на функцию для сравнения элементов массива.
 */
-void qsort (void* array, int start, int end, int (*comp) (void*, void*));
+void qsort (void* array, size_t n_memb,  size_t el_size,
+            int (*comparator) (const void*, const void*));
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
@@ -35,12 +38,18 @@ void qsort (void* array, int start, int end, int (*comp) (void*, void*));
 * @details Данная функция используется внутри функции @link qsort @endlink, 
 *          как часть алгоритма быстрой сортировки.
 *
-* @param [in] array Указатель на сортируемый массив.
-* @param [in] start Указатель на левую границу сортируемого участка.
-* @param [in] end Указатель на правую границу сортируемого участка.
-* @param [in] pivot Опорный элемент.
+* @param [in, out] array Указатель на массив указателей для сортировки.
+* @param [in] n_memb Количество элементов в массиве.
+* @param [in] el_size Размер одного элемента в байтах.
+* @param [in, out] left Указатель на левую границу сортируемого участка (индекс элемента массива).
+* @param [in, out] right Указатель на правую границу сортируемого участка (индекс элемента массива).
+* @param [in] pivot_index Индекс опорного элемента.
+* @param [in] comparator Указатель на функцию для сравнения элементов массива.
+*
+* @return Возвращает индекс последнего элемента левой части массива.
 */
-void paritation (void* array, int* start, int* end, int pivot);
+unsigned paritation (void* array, size_t n_memb, size_t el_size, unsigned pivot_index, 
+                     int (*comparator) (const void*, const void*));
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
@@ -49,8 +58,19 @@ void paritation (void* array, int* start, int* end, int pivot);
 *
 * @param [in,out] a Указатель на первый элемент.
 * @param [in,out] b Указатель на второй элемент.
+* @param [in] el_size Размер одного элемента в байтах.
 */
-void swap (void* a, void* b);
+void swap (void* a, void* b, size_t el_size);
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+/**
+* @brief Функция choose_pivot выбирает в массиве опорный элемент.
+*
+* @param [in] n_memb Количество элементов в массиве.
+* @return Индекс опорного элемента в массиве
+*/
+unsigned choose_pivot (size_t n_memb);
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
@@ -64,7 +84,7 @@ void swap (void* a, void* b);
 *         0, если string_1 == string_2,
 *         1, если string_1 > string_2
 */
-int lexicographic_comparator (void* string_1, void* string_2);
+int lexicographic_comparator (const void* string_1, const void* string_2);
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
