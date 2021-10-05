9ea827388e1bb19811f6d89f70d895e4d86ef724
Danny <bublikplushka@yandex.ru>
2021-10-05 21:18:00 +0300

    Fix errors, improve stack.
    Rewrite hashing,
    Add multiple errors system
    Fix constructor
    Add AdditionalInfo struct to stack
    Rewrite StackDump and StackValidate
    Add Validation to every function but only as conditional compilation
    ---------------------------------------------
    Андрей, если ты это читаешь, то ставь 10 баллов
    ---------------------------------------------
    Expand UnitTests

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -8,7 +8,10 @@ int StackCtorFunc (Stack* stack, size_t size, const char* stack_name, const char
 {   
     assert (stack);
     assert (size);
-    
+    assert (func_name);
+    assert (file_name);
+    assert (stack_name);
+
     if (stack->data != NULL)
     {
         return STACK_CTOR_DBL_CALL;
@@ -18,22 +21,28 @@ int StackCtorFunc (Stack* stack, size_t size, const char* stack_name, const char
 
     if (!tmp_pointer)
     {   
-        stack->error_code = STACK_CTOR_CODE + STACK_MEM_ALLOCK_ERR;
+        stack->info.error_code = stack->info.error_code | STACK_MEM_ALLOCK_ERR;
+        stack->info.error_func_code = stack->info.error_func_code | STACK_CTOR_CODE;
+
         return STACK_MEM_ALLOCK_ERR;
     }
 
     stack->start_bird = HUMMINGBIRD;
-    stack->data = tmp_pointer;
-    stack->init_func = func_name;
-    stack->stack_name = stack_name;
-    stack->line = line;
-    stack->filename = file_name;
     stack->size = 0;
     stack->capacity = size;
-    stack->end_bird = HUMMINGBIRD;
+    stack->data = tmp_pointer;
     stack->hash = StackHashSum (stack);
+    stack->end_bird = HUMMINGBIRD;
 
-    return STACK_OK;
+    stack->info.print_function = NULL;
+    stack->info.init_func = func_name;
+    stack->info.stack_name = stack_name;
+    stack->info.line = line;
+    stack->info.filename = file_name;
+    stack->info.error_code = STACK_OK;
+    stack->info.error_func_code = 0;
+
+    return StackValidate (stack, STACK_CTOR_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -43,22 +52,30 @@ int StackDtor (Stack* stack)
     assert (stack);
 
     if (!stack->data)
-    {
+    {   
+        stack->info.error_code = stack->info.error_code | STACK_DTOR_DBL_CALL;
+        stack->info.error_func_code = stack->info.error_func_code | STACK_DTOR_CODE;
+
         return STACK_DTOR_DBL_CALL;
     }
 
     memset (stack->data, STACK_DATA_POISON, stack->capacity);
     free (stack->data);
 
-    return STACK_OK;
+    return ValidateResult (stack, STACK_DTOR_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 int SetStackPrinterFunc (Stack* stack, void (*printer_func)(stack_type*))
-{
-    stack->print_function = printer_func;
-    return STACK_OK;
+{   
+    assert (stack);
+    assert (printer_func);
+
+    stack->info.print_function = printer_func;
+
+    return ValidateResult (stack, SET_STACK_PRINTER_CODE);
+
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -66,23 +83,32 @@ int SetStackPrinterFunc (Stack* stack, void (*printer_func)(stack_type*))
 int StackPush (Stack* stack, stack_type element)
 {
     assert (stack);
+    assert(stack->data);
+
     int err_code = 0;
 
     if ((err_code = StackValidate (stack, STACK_PUSH_CODE)) != STACK_OK)
-    {
+    {   
+        stack->info.error_code = stack->info.error_code | err_code;
+        stack->info.error_func_code = stack->info.error_func_code | STACK_PUSH_CODE;
+
         StackDump (stack);
+
         return err_code;
     }
 
     if (StackIncrease (stack) != STACK_OK)
-    {
+    {   
+        stack->info.error_code = stack->info.error_code | STACK_INCREASE_ERR;
+        stack->info.error_func_code = stack->info.error_func_code | STACK_PUSH_CODE;
+
         return STACK_INCREASE_ERR;
     }
 
     stack->data[stack->size++] = element;
     stack->hash = StackHashSum (stack);
 
-    return STACK_OK;
+    return ValidateResult (stack, STACK_PUSH_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -90,25 +116,32 @@ int StackPush (Stack* stack, stack_type element)
 int StackPop (Stack* stack, stack_type* out_element)
 {
     assert (stack);
-    
+    assert (out_element);
+
     int err_code = 0;
 
     if ((err_code = StackValidate (stack, STACK_POP_CODE)) != STACK_OK)
-    {
+    {   
+        stack->info.error_code = stack->info.error_code | err_code;
+        stack->info.error_func_code = stack->info.error_func_code | STACK_POP_CODE;
+
         StackDump (stack);
         return err_code;
     }
 
     if (StackDecrease (stack) != STACK_OK)
-    {
+    {   
+        stack->info.error_code = stack->info.error_code | STACK_DECREASE_ERR;
+        stack->info.error_func_code = stack->info.error_func_code | STACK_POP_CODE;
+
         return STACK_DECREASE_ERR;
     }
 
     *out_element = stack->data[--stack->size];
-    stack->data[stack->size] = STACK_DATA_POISON; //TODO чем заполнить??
+    stack->data[stack->size] = STACK_DATA_POISON;
     stack->hash = StackHashSum (stack);
 
-    return STACK_OK;
+    return ValidateResult (stack, STACK_POP_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -124,7 +157,8 @@ int StackIncrease (Stack* stack)
 
         if (!tmp)
         {
-            stack->error_code = STACK_MEM_ALLOCK_ERR + STACK_INCREASE_CODE;
+            stack->info.error_code = stack->info.error_code | STACK_MEM_ALLOCK_ERR;
+            stack->info.error_func_code = stack->info.error_func_code | STACK_INCREASE_CODE;
 
             return STACK_MEM_ALLOCK_ERR;
         }
@@ -133,7 +167,7 @@ int StackIncrease (Stack* stack)
         stack->capacity = 2 * stack->capacity;
     }
 
-    return STACK_OK;
+    return ValidateResult (stack, STACK_INCREASE_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -148,8 +182,9 @@ int StackDecrease (Stack* stack)
         stack_type* tmp = (stack_type*) realloc (stack->data, stack->capacity / 2 + 1);
 
         if (!tmp)
-        {
-            stack->error_code = STACK_MEM_ALLOCK_ERR + STACK_INCREASE_CODE;
+        {   
+            stack->info.error_code = stack->info.error_code | STACK_MEM_ALLOCK_ERR;
+            stack->info.error_func_code = stack->info.error_func_code | STACK_DECREASE_CODE;
 
             return STACK_MEM_ALLOCK_ERR;
         }
@@ -158,7 +193,7 @@ int StackDecrease (Stack* stack)
         stack->capacity = stack->capacity / 2 + 1;
     }
 
-    return STACK_OK;
+    return ValidateResult (stack, STACK_DECREASE_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -167,61 +202,65 @@ int StackValidate (Stack* stack, int func_code)
 {
     assert (stack);
 
-     if(stack->data == NULL)
+    if(stack->data == NULL)
     {
-        stack->error_code = STACK_INCORRECT_DATA_PTR + func_code;
+        stack->info.error_code = stack->info.error_code | STACK_INCORRECT_DATA_PTR;
     }
 
     if(stack->start_bird != HUMMINGBIRD)
     {
-        stack->error_code = STACK_WRONG_START_HUMMINGBIRD + func_code;
+        stack->info.error_code = stack->info.error_code | STACK_WRONG_START_HUMMINGBIRD;
     }
 
     if(stack->start_bird != HUMMINGBIRD)
     {
-        stack->error_code = STACK_WRONG_END_HUMMINGBIRD + func_code;
+        stack->info.error_code = stack->info.error_code |  STACK_WRONG_END_HUMMINGBIRD;
     }
 
     if(stack->size > stack->capacity)
     {
-        stack->error_code = STACK_INCORRECT_SIZE + func_code;
+        stack->info.error_code = stack->info.error_code |  STACK_INCORRECT_SIZE;
     }
 
-    if(stack->hash != StackHashSum(stack))
+    if(CheckStackHash (stack) != 1)
     {
-        stack->error_code = STACK_WRONG_HASH_SUM + func_code;
+        stack->info.error_code = stack->info.error_code |  STACK_WRONG_HASH_SUM;
     }
 
-    return stack->error_code;
+    stack->info.error_func_code = stack->info.error_func_code |  STACK_VALIDATE_CODE;
+    stack->info.error_func_code   = stack->info.error_func_code |  func_code;
+
+    return stack->info.error_code;
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 int StackDumpFunc (Stack* stack, const int line, const char* func_name, const char* file_name, const char* stack_name)
-{
-    int err_and_func_code = StackValidate (stack, STACK_DUMP_CODE);
-    int err_code = err_and_func_code % 256;
+{   
+    assert (stack);
+    assert (func_name);
+    assert (file_name);
+    assert (stack_name);
 
-    printf ("STACK \"%s\" was init. in func. %s line %d file %s \n", 
-            stack->stack_name, stack->init_func, stack->line, stack->filename);
+    int err_code = StackValidate (stack, STACK_DUMP_CODE);
 
     if (err_code == STACK_OK)
     {
-        printf (GREEN_COLOR("[OK]: "));
+        printf (GREEN_COLOR("\nSTACK [OK]: "));
     }
     
     else
     {
-        printf (RED_COLOR("[ERROR][CODE: 0x%X]"), err_code);
+        printf (RED_COLOR("\nSTACK [ERROR][ERR_CODE: 0x%X]"), err_code);
     }
 
     printf ("[%p] ", &stack->data);
-
-    printf ("Dump for %s called in func. %s, line %d, file %s'\n", stack_name, func_name, line, file_name);
+    printf ("\"%s\" was init. in func. %s line %d file %s \n", stack->info.stack_name, stack->info.init_func, stack->info.line, stack->info.filename);
+    printf ("Dump for \"%s\" called in func. %s, line %d, file %s'\n", stack_name, func_name, line, file_name);
  
     if(err_code != STACK_OK)
-    {   
-        StackPrintExitCode(err_and_func_code);
+    {
+        StackPrintExitCode(stack);
     }
 
     printf ("\n{\n");              
@@ -249,16 +288,15 @@ int StackDumpFunc (Stack* stack, const int line, const char* func_name, const ch
 
         printf  ("[%lu] = ", i);
 
-        if (stack->print_function)
+        if (stack->info.print_function)
         {
-            stack->print_function (stack->data + i);
+            stack->info.print_function (stack->data + i);
         }
         else
         {   
-            //TODO перепроверить побитовую печать
-            char* data_ptr = (char*) (stack->data + i);
+            unsigned char* data_ptr = (unsigned char*) (stack->data + i);
 
-            for (size_t j = 0; j < sizeof (stack_type); j++)
+            for (size_t j = 0; j <  sizeof (stack_type); j++)
             {
                 printf ("%02X ", data_ptr[j]);
             }
@@ -269,89 +307,170 @@ int StackDumpFunc (Stack* stack, const int line, const char* func_name, const ch
 
     printf ("   }\n}\n");
 
-    return STACK_OK;
+    return ValidateResult (stack, STACK_DUMP_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-int StackPrintExitCode(int err_and_func_code)
+int StackPrintExitCode(Stack* stack)
 {   
-    int err_code = err_and_func_code % 256;
-    int func_code = err_and_func_code / 256;
-    
-    if (err_code == STACK_OK)
+    if (stack->info.error_code == STACK_OK)
     {
-        printf ("No errors founded\n");
-        return 0;
+        printf (GREEN_COLOR("No errors founded\n"));
+        return ValidateResult (stack, STACK_PRINT_EXIT_CODE_CODE);
+    } 
+
+    int err_code = stack->info.error_code;
+    int func_code = stack->info.error_func_code;
+    printf ("\nThere are some errors in stack: \n");
+
+    if (err_code & STACK_MEM_ALLOCK_ERR)
+    {
+        printf (RED_COLOR("Memory allocation error\n"));
+        err_code -= STACK_MEM_ALLOCK_ERR;
     }
 
-    switch (err_code)
+    if (err_code & STACK_INCORRECT_DATA_PTR)
     {
-        case STACK_MEM_ALLOCK_ERR:
-            printf ("Memory allocation error in ");
-            break;
-    
-        case STACK_INCORRECT_DATA_PTR:
-            printf ("Incorrect pointer to start of buffer founded in ");
-            break;
-
-        case STACK_INCORRECT_SIZE:
-            printf ("Stack size below zero or stack overdlow exception founded in ");
-            break;
-
-        case STACK_INCORRECT_CAPACITY:
-            printf ("Stack capacity below zeor  founded in ");
-            break;
-
-        case STACK_WRONG_START_HUMMINGBIRD:
-            printf ("Changes in start hummingbird founded in ");
-            break;
-
-        case STACK_WRONG_END_HUMMINGBIRD:
-            printf ("Changes in end hummingbird founded in ");
-            break;
-
-        case STACK_WRONG_HASH_SUM:
-            printf ("Mismacth of hash sum founded in ");
-            break;
-
-        default:
-            printf ("Unkwonw exception with number %X in ", err_code);
-            break;
+        printf (RED_COLOR("Incorrect pointer to start of buffer founded\n"));
+        err_code -= STACK_INCORRECT_DATA_PTR;
     }
 
-    switch (func_code)
+    if (err_code & STACK_WRONG_START_HUMMINGBIRD)
     {
-        case STACK_CTOR_CODE:
-            printf ("func StackCtor()\n");
-            break;
-
-        case STACK_PUSH_CODE:
-            printf ("func StackPush()\n");
-            break;
-
-        case STACK_POP_CODE:
-            printf ("func StackPop()\n");
-            break;
-
-        case STACK_INCREASE_CODE:
-            printf ("func StackIncrease()\n");
-            break;
-
-        case STACK_DECREASE_CODE:
-            printf ("func StackDecrease()\n");
-            break;
-
-        case STACK_DUMP_CODE:
-            printf ("macro StackDump()\n");
-            break;
-
-        default:
-            printf ("unkwonw func with number %X\n", func_code);
-            break;
+        printf (RED_COLOR("Changes in start hummingbird founded\n"));
+        err_code -= STACK_WRONG_START_HUMMINGBIRD;
     }
 
-    return 0;
+    if (err_code & STACK_WRONG_END_HUMMINGBIRD)
+    {
+        printf (RED_COLOR("Changes in end hummingbird founded\n")); 
+        err_code -= STACK_WRONG_END_HUMMINGBIRD;
+    }
+
+    if (err_code & STACK_WRONG_HASH_SUM)
+    {
+        printf (RED_COLOR("Mismacth of hash sum founded\n"));
+        err_code -= STACK_WRONG_HASH_SUM;
+    }
+
+    if (err_code & STACK_CTOR_DBL_CALL)
+    {
+        printf (RED_COLOR("Stack constructor was called twice\n"));
+        err_code -= STACK_CTOR_DBL_CALL;
+    }
+
+    if (err_code & STACK_DTOR_DBL_CALL)
+    {
+        printf (RED_COLOR("Stack destructor was called twice\n"));
+        err_code -= STACK_DTOR_DBL_CALL;
+    }
+
+    if (err_code & STACK_INCORRECT_CAPACITY)
+    {
+        printf (RED_COLOR("Stack capcity is incorrect\n"));
+        err_code -= STACK_INCORRECT_CAPACITY;
+    }
+
+    if (err_code & STACK_INCORRECT_SIZE)
+    {
+        printf (RED_COLOR("Stack size is incorrect\n"));
+        err_code -= STACK_INCORRECT_SIZE;
+    }
+
+    if (err_code & STACK_INCREASE_ERR)
+    {
+        printf (RED_COLOR("Error while increasing stack\n"));
+        err_code -= STACK_INCREASE_ERR;
+    }
+
+    if (err_code & STACK_DECREASE_ERR)
+    {
+        printf (RED_COLOR("Error while decreasing stack\n"));
+        err_code -= STACK_DECREASE_ERR;
+    }
+
+    if (err_code != 0)
+    {
+        printf (RED_COLOR("Unkwonw exception with code %X \n"), err_code);
+    }
+
+    printf ("\nErrors were reached in next function(s): \n");
+
+    if (func_code & STACK_CTOR_CODE)
+    {
+        printf (RED_COLOR("StackCtor()\n"));
+        func_code -= STACK_CTOR_CODE;
+    }
+
+    if (func_code & STACK_DTOR_CODE)
+    {
+        printf (RED_COLOR("StackDtor()\n"));
+        func_code -= STACK_DTOR_CODE;
+    }
+
+    if (func_code & STACK_PUSH_CODE)
+    {
+        printf (RED_COLOR("StackPush()\n"));
+        func_code -= STACK_PUSH_CODE;
+    }
+
+    if (func_code & STACK_POP_CODE)
+    {
+        printf (RED_COLOR("StackPop()\n"));
+        func_code -= STACK_POP_CODE;
+    }
+
+    if (func_code & STACK_INCREASE_CODE)
+    {
+        printf (RED_COLOR("StackIncrease()\n"));
+        func_code -= STACK_INCREASE_CODE;
+    }
+
+    if (func_code & STACK_DECREASE_CODE)
+    {
+        printf (RED_COLOR("StackDecrease()\n"));
+        func_code -= STACK_DECREASE_CODE;
+    }
+
+    if (func_code & STACK_DUMP_CODE)
+    {
+        printf (RED_COLOR("StackDump()\n"));
+        func_code -= STACK_DUMP_CODE;
+    }
+
+    if (func_code & STACK_UNIT_TEST_CODE)
+    {
+        printf (RED_COLOR("UnitTest()\n"));
+        func_code -= STACK_UNIT_TEST_CODE;
+    }
+
+    if (func_code & STACK_SET_PRINTER_CODE)
+    {
+        printf (RED_COLOR("SetStackPrinter()\n"));
+        func_code -= STACK_SET_PRINTER_CODE;
+    }
+
+    if (func_code & STACK_PRINT_EXIT_CODE_CODE)
+    {
+        printf (RED_COLOR("PrintExitCode()\n"));
+        func_code -= STACK_PRINT_EXIT_CODE_CODE;
+    }
+
+    if (func_code & STACK_VALIDATE_CODE)
+    {
+        printf (RED_COLOR("StackValidate()\n"));
+        func_code -= STACK_VALIDATE_CODE;
+    }
+
+    if (func_code != 0)
+    {
+        printf (RED_COLOR("Unkwonw function with code: %X\n"), func_code);
+    }
+
+    printf ("\n");
+
+    return ValidateResult (stack, STACK_PRINT_EXIT_CODE_CODE);
 }
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -376,18 +495,31 @@ unsigned long long HashSum(void* pointer, size_t size, unsigned long long hash)
 unsigned long long StackHashSum(Stack* stack)
 {
     assert(stack);
-
-    size_t first_part_size =  sizeof (Stack) - sizeof (hummingbird_type) - sizeof (unsigned long long);
-
-    unsigned long long struct_hash_1st_part = HashSum (stack, first_part_size);
-    unsigned long long hash = HashSum (&stack->end_bird, sizeof(hummingbird_type), struct_hash_1st_part);
+    //TODO норм или не норм?
+    stack->hash = 0;
+    unsigned long long hash = HashSum (stack, sizeof(stack));
 
     #ifdef DATA_HASHING
     hash = HashSum (stack->data, stack->size * sizeof (stack_type), hash);
     #endif
-
+    
     return hash;
 }
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+int CheckStackHash (Stack* stack)
+{
+    unsigned long long old_hash = stack->hash;
+    unsigned long long cur_hash = StackHashSum (stack);
+
+    //TODO норм или не норм?
+    stack->hash = old_hash;
+
+    return stack->hash == cur_hash;
+}
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 void stack_hex_printer (stack_type* data)
 {
@@ -395,23 +527,25 @@ void stack_hex_printer (stack_type* data)
 }
 
 int UnitTest ()
-{
+{   
     Stack stack1 = {};
     StackCtor (&stack1, 10);
-    SetStackPrinterFunc (&stack1, stack_hex_printer);
-
     StackDump (&stack1);
-    StackPush(&stack1, 7);
+
+    StackPush(&stack1, 256);
     StackPush(&stack1, 8);
     StackPush(&stack1, 9);
-    StackPush(&stack1, 16384);
+    StackPush(&stack1, 0xFFFFFFAF);
     StackDump (&stack1);
 
     int el = 666;
     StackPop (&stack1, &el);
+    SetStackPrinterFunc (&stack1, stack_hex_printer);
+
+    stack1.info.error_code = 0x000001C3;
+    stack1.info.error_func_code = 0x000004C3;
     StackDump (&stack1);
 
-    printf ("\n%d\n", el);
     StackDtor (&stack1);
 
     return 0;
--- stack/stack.h
+++ stack/stack.h
@@ -7,8 +7,8 @@
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-#define GREEN_COLOR(text) "\033[92m" #text "\033[0m"
-#define RED_COLOR(text) "\033[91m" #text "\033[0m"
+#define GREEN_COLOR(text) ("\033[92m" text "\033[0m")
+#define RED_COLOR(text) ("\033[91m" text "\033[0m")
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
@@ -20,6 +20,18 @@
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+#ifndef STACK_DATA_POISON
+#define STACK_DATA_POISON 0xDEAD
+#endif
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+#ifdef VALIDATE_ALL
+#define ValidateResult(stack, func_code) StackValidate(stack, func_code)
+#else
+#define ValidateResult(stack, func_code) STACK_OK
+#endif
+
 /// Объявление типа стака. Необходимо, чтобы стак был независим от типа данных.
 typedef int stack_type;
 typedef unsigned long long hummingbird_type;
@@ -29,23 +41,34 @@ const hummingbird_type HUMMINGBIRD = (hummingbird_type) 0xDEAD0DED;
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+/** @struct AdditionalInfo
+ *  @brief Дополнительные данные стека.
+ */
+typedef struct AdditionalInfo
+{       
+    int error_code;                         /**< Поле AdditionalInfo#error_code хранит код ошибки. */
+    int error_func_code;                    /**< Поле AdditionalInfo#error_code хранит код функции, в которой произошла ошибка. */
+    const char* filename;                   /**< Поле AdditionalInfo#filename хранит имя файла, в котором был проинициализирован стек. */
+    int line;                               /**< Поле AdditionalInfo#line хранит номер строки, в которой был проинициализирован стек. */
+    const char* stack_name;                 /**< Поле AdditionalInfo#stack_name хранит имя объекта стека. */
+    const char* init_func;                  /**< Поле AdditionalInfo#stack_name хранит имя функции, в которой был проинициализирован стек. */
+    void (*print_function)(stack_type*);    /**< Поле AdditionalInfo#print_function хранит указатель на функцию для распечатки значений. */
+} AdditionalInfo;
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 /** @struct Stack
  *  @brief Структура стека.
  */
 typedef struct Stack
-{   
-    hummingbird_type start_bird;
-    size_t size;                /**< Поле Stack#size хранит размер занятого пространства. */
-    size_t capacity;            /**< Поле Stack#capacity хранит размер выделенной памяти. */
-    stack_type* data;           /**< Поле Stack#data хранит указатель на начало массива с данными. */
-    int error_code;             /**< Поле Stack#error_code хранит ошибку. */
-    const char* filename;       /**< Поле Stack#filename хранит имя файла, в котором был проинициализирован стек. */
-    int line;                   /**< Поле Stack#line хранит номер строки, в которой был проинициализирован стек. */
-    const char* stack_name;     /**< Поле Stack#stack_name хранит имя объекта стека. */
-    const char* init_func;
-    void (*print_function)(stack_type*);
-    unsigned long long hash;
-    hummingbird_type end_bird;
+{       
+    hummingbird_type start_bird;            /**< Поле Stack#start_bird хранит канарейку начала структуры. */
+    size_t size;                            /**< Поле Stack#size хранит размер занятого пространства. */
+    size_t capacity;                        /**< Поле Stack#capacity хранит размер выделенной памяти. */
+    stack_type* data;                       /**< Поле Stack#data хранит указатель на начало массива с данными. */
+    unsigned long long hash;                /**< Поле Stack#hash хранит хэш стека.*/
+    AdditionalInfo info;                    /**< Поле Stack#info хранит дополнительную информацию о структуре. */
+    hummingbird_type end_bird;              /**< Поле Stack#end_bird хранит канарейку конца структуры. */
 } Stack;
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -55,18 +78,18 @@ typedef struct Stack
 */
 enum RETURN_CODES
 {
-    STACK_OK =                  0x00000000,     // Все ОК.
-    STACK_MEM_ALLOCK_ERR =      0x00000001,     // Ошибка аллоцирования / реаллоцирования памяти.
-    STACK_INCREASE_ERR =        0x00000002,     // Ошибка в функции увеличения размера стека.
-    STACK_DECREASE_ERR =        0x00000003,     // Ошибка в функции уменьшения размера стека.
-    STACK_CTOR_DBL_CALL =       0x00000004,     // Двойной вызов конструктора.
-    STACK_DTOR_DBL_CALL =       0x00000005,     // Двойной вызов деструктора.
-    STACK_INCORRECT_DATA_PTR =  0x00000006,     //  Неверный указатель на данные.
-    STACK_WRONG_START_HUMMINGBIRD = 0x00000007,  //  Неверная начальная канарейка.
-    STACK_WRONG_END_HUMMINGBIRD = 0x00000008,  //  Неверная конечная канарейка.
-    STACK_INCORRECT_SIZE = 0x00000009,  //  Неверный размер стека.
-    STACK_INCORRECT_CAPACITY = 0x00000010,  //  Неверный размер стека.
-    STACK_WRONG_HASH_SUM = 0x00000011,  //  Неверный размер стека.
+    STACK_OK =                          0x00000000,     // Все ОК.
+    STACK_MEM_ALLOCK_ERR =              0x00000001,     // Ошибка аллоцирования / реаллоцирования памяти.
+    STACK_INCREASE_ERR =                0x00000002,     // Ошибка в функции увеличения размера стека.
+    STACK_DECREASE_ERR =                0x00000004,     // Ошибка в функции уменьшения размера стека.
+    STACK_CTOR_DBL_CALL =               0x00000008,     // Двойной вызов конструктора.
+    STACK_DTOR_DBL_CALL =               0x00000010,     // Двойной вызов деструктора.
+    STACK_INCORRECT_DATA_PTR =          0x00000020,     //  Неверный указатель на данные.
+    STACK_WRONG_START_HUMMINGBIRD =     0x00000040,     //  Неверная начальная канарейка.
+    STACK_WRONG_END_HUMMINGBIRD =       0x00000080,     //  Неверная конечная канарейка.
+    STACK_INCORRECT_SIZE =              0x00000100,     //  Неверный размер стека.
+    STACK_INCORRECT_CAPACITY =          0x00000200,     //  Неверный размер стека.
+    STACK_WRONG_HASH_SUM =              0x00000400      //  Неверный размер стека.
 };
 
 /**
@@ -74,22 +97,17 @@ enum RETURN_CODES
 */
 enum FUNC_CODES
 {
-    STACK_CTOR_CODE  =           0x00000100,
-    STACK_DTOR_CODE =            0x00000200,
-    STACK_PUSH_CODE =            0x00000300,
-    STACK_POP_CODE =             0x00000400,
-    STACK_INCREASE_CODE =        0x00000500,
-    STACK_DECREASE_CODE =        0x00000600,
-    STACK_DUMP_CODE =            0x00000700,
-    UNIT_TEST_CODE =             0x00000800
-};
-
-/**
-*   Перечисление отравляющих констант.  
-*/
-enum POISONS
-{
-    STACK_DATA_POISON = 0xDEAD // Отравление данных.
+    STACK_CTOR_CODE  =           0x00000001,
+    STACK_DTOR_CODE =            0x00000002,
+    STACK_PUSH_CODE =            0x00000004,
+    STACK_POP_CODE =             0x00000008,
+    STACK_INCREASE_CODE =        0x00000010,
+    STACK_DECREASE_CODE =        0x00000020,
+    STACK_DUMP_CODE =            0x00000040,
+    STACK_UNIT_TEST_CODE =       0x00000080,
+    STACK_SET_PRINTER_CODE =     0x00000100,
+    STACK_PRINT_EXIT_CODE_CODE = 0x00000200,
+    STACK_VALIDATE_CODE =        0x00000400
 };
 
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -217,11 +235,21 @@ unsigned long long StackHashSum(Stack* stack);
 //flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 /**
+*   @brief Функция CheckStackHash проверяет хэш-сумму стека на корректность.
+*   
+*   @param [in] stack Указатель на структуру \link Stack \endlink.
+*   @return 1, если хэш-сумма текущего состояния стека равна сохраненной хэш-сумме, иначе 0
+*/
+int CheckStackHash (Stack* stack);
+
+//flexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+/**
 *   @brief Функция StackPrintExitCode .
 *   
-*   @param [in] err_and_func_code Переменная, содержащая код функции и ошибки.
+*   @param [in] stack Указатель на объект стека.
 */
-int StackPrintExitCode(int err_and_func_code);
+int StackPrintExitCode(Stack* stack);
 
 /**
 *   @brief Функция UnitTest проводит тестирование стака.
